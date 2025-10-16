06f1a81e93d68d831445e55976846e23fb2b0fcb
rahul kahar 88 <rahulkahar88588@gmail.com>
2025-10-16 16:25:56 +0530

    rk

---
+++ project.js
@@ -0,0 +1,28 @@
+
+// let a = 1
+// function fun(){   
+//    setInterval(()=>{
+//     alert(" alert  "+a)
+//     ++a      
+// },3000)
+// }
+
+
+
+function telegram(){
+   let t = document.querySelector('#h1')
+   t.style.color='pink'
+   t.textContent='telegram'
+}
+function facebook(){
+   let f = document.querySelector('#h1')
+   f.style.color='blue'
+   f.textContent='facebook'
+}
+function youtube(){
+   let y = document.querySelector('#h1')
+   y.style.color='red'
+   y.textContent='youtube'
+}
+
+
