c865de17375757d72660ee5982f4f2b36de1f55a
rahul kahar 88 <rahulkahar88588@gmail.com>
2025-11-10 20:48:04 +0530

    update

---
+++ API.js
@@ -0,0 +1,17 @@
+
+
+//   API   --   application programming interface
+        
+// fatch(url,{method})
+
+
+
+
+// HTTP  - hyper text transfer protocol
+
+// http method =================
+//  get  ---  access
+//  post ---  insert
+//  delete  --- delete
+//  put  --- update
+//  patch  --- 
---
+++ string.js
@@ -0,0 +1,51 @@
+// let name = "rahul kahar"
+//   let n =    name[4]                    
+//   console.log(n);
+  
+
+
+
+// let name = "rahul kahar"
+// let city = "Gorakhpur"
+//   let newname = name.toUpperCase()                // toUpperCase() use to set all character in capital
+//   console.log(newname);  
+//   console.log(city.toLowerCase())                 // toLowerCase() use to set all character in small
+
+
+
+
+
+
+// let name = " shiva "
+// console.log(name.trim())                           // trim() use to remove space
+// console.log(name.trimStart())                      // trimStart() use to remove starting space
+// console.log(name.trimEnd())                        // trimStart() use to remove ending space
+
+
+
+
+
+
+
+// let faculty = "by rahul"
+// let st = "this is \n js class" + " " + faculty
+// console.log(st)
+
+// template literals   ( ` ` )
+
+// let st1 = `this is javascript
+// class  ${faculty}`
+// console.log(st1)
+// 
+
+
+
+
+ 
+
+
+
+
+
+
+
