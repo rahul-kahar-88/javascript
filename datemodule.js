30a452097835ed0d171d44d31bb9e276370988df
rahul kahar 88 <rahulkahar88588@gmail.com>
2025-11-12 17:41:54 +0530

    update

---
+++ datemodule.js
@@ -0,0 +1,15 @@
+ 
+ 
+//      Datemodule 
+
+
+ 
+ 
+ let today = new Date ()
+ console.log(today)
+
+ console.log(today.getFullYear())
+ console.log(today.getMonth()+1)
+ console.log(today.getDate())
+ console.log(today.toLocaleTimeString())
+ 
---
+++ mathmodule.js
@@ -0,0 +1,30 @@
+
+
+// Math Module
+
+
+
+
+// console.log(Math.pow(3,2))
+// console.log(Math.sqrt(16))
+// console.log(Math.PI)
+// console.log(Math.min(4,8,6,10,3,2))
+// console.log(Math.max(5,8,1,10,3,2))
+// console.log(Math.trunc(16.45))
+
+
+// console.log(Math.random())   // - it give random value 0 - 1
+
+// console.log(Math.random()*10+1)   // - it give random value 1 - 10 
+
+// console.log(Math.trunc(Math.random()*10+1))    // - it give random value integer 
+
+
+
+
+
+
+ 
+ 
+ 
+ 
